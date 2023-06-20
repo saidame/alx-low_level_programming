95db7034bc5941bf7b4f9f024728546420ea00bd
saidame <said.amchghal@example.com>
2023-06-20 05:13:54 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+int main(void){
+	char str[]="_putchar";
+	int ch;
+	for(ch=0; ch<8; ch++)
+		_putchar(str[ch]);
+		_putchar('\n');
+		retur(0);
+	}
