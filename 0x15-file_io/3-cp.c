158634e87fc1b76b1b2ea5420f35b0776be5288a
saidame <said.amchghal@example.com>
2023-08-07 04:24:07 -0700

    m

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
 
 	buffer = create_buffer(argv[2]);
 	fr = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	r = read(fr, buffer, 1024);
 	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
