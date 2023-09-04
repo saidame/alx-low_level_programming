a7f9492ab7f6398ca5582950589b42b79c7e4e9b
saidame <said.amchghal@gmail.com>
2023-09-04 05:01:44 -0700

    m

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+i#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read text file and print to STDOUT.
+ * @filename: The text file to be read.
+ * @letters: The number of letters to be read.
+ * Return: The actual number of bytes read and printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_written;
+	ssize_t total_bytes_written;
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
+
+	total_bytes_written = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, total_bytes_written);
+
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
+}
